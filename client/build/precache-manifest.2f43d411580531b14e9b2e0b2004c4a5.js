edd2839e9d4d9ec78d6b70d571896a6fde8217ca
dthompson999 <63125447+dthompson999@users.noreply.github.com>
2020-09-01 19:02:59 -0500

    added build

---
+++ client/build/precache-manifest.2f43d411580531b14e9b2e0b2004c4a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a75a01fe48e7ad6060e143f5d9cdf45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61e2b26e125f585a38cc",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "781c07f5418f5b18fb5d",
+    "url": "/static/css/main.b553db30.chunk.css"
+  },
+  {
+    "revision": "61e2b26e125f585a38cc",
+    "url": "/static/js/2.67550c18.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/static/js/2.67550c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "781c07f5418f5b18fb5d",
+    "url": "/static/js/main.524e8689.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2433948fee010fc7da5e4dc8fa24db74",
+    "url": "/static/media/citymap.2433948f.png"
+  }
+]);
